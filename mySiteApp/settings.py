08fdfd240890e6a1cec2cedb18a3d4181ca2482b
AhasEko <ahaseko1@gmail.com>
2023-02-07 09:52:40 +0700

    fix

--- mySiteApp/settings.py
+++ mySiteApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kdwgjzb2(vl4j#(2e)w$m5w7++n-v^09j&#q+x#qa%hk+2mohp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
